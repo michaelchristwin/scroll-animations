bb4615f0ebb8502cd4af74678b32b527db914b8b
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-04-23 17:32:26 +0100

    chore: default codepen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { onMount } from "solid-js";
 import Marquee from "./components/Marquee";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from "gsap";
 
 export default function App() {
   let scrollerRef!: HTMLElement;
-
+  onMount(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.defaults({
+      scroller: scrollerRef,
+    });
+  });
   return (
     <main class={`h-[100vh] overflow-y-scroll`} ref={scrollerRef}>
-      {/* <Hero />
-      <HorizontalGallery /> */}
       <Marquee scroller={scrollerRef} />
-      {/* <Sticky /> */}
     </main>
   );
 }
--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,145 +1,84 @@
-import { Component, For, onCleanup, onMount } from "solid-js";
-import { createTimeline, Timeline, onScroll } from "animejs";
+import { Component, onCleanup, onMount } from "solid-js";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 type MarqueeProps = {
   scroller: HTMLElement;
 };
-const Marquee: Component<MarqueeProps> = (props) => {
+const Marquee: Component<MarqueeProps> = () => {
   let section2Ref!: HTMLDivElement;
-  let leftCol!: HTMLDivElement;
-  let rightCol!: HTMLDivElement;
-  let stickyContainer!: HTMLDivElement;
-  let timeline: Timeline;
+  let ctx: gsap.Context;
 
   onMount(() => {
-    timeline = createTimeline({
-      defaults: {
-        ease: "linear",
-        duration: 500,
-        composition: "blend",
-      },
-      autoplay: onScroll({
-        target: stickyContainer,
-        container: props.scroller,
-        enter: "top top",
-        leave: "bottom bottom",
-        sync: 0.5,
-      }),
-    });
-    timeline
-      .add(leftCol, {
-        translateY: ["0%", "-50%"],
-        duration: 1000,
-      })
-      .add(
-        rightCol,
+    ctx = gsap.context(() => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: section2Ref,
+          start: "0% 0%",
+          end: "120% 0%",
+          scrub: 1,
+          pin: true,
+        },
+      });
+
+      tl.to(
+        ".images .right",
         {
-          translateY: ["0%", "50%"],
-          duration: 1000,
+          marginTop: "0",
+
+          duration: 1,
         },
         0
       );
-    timeline.init();
+      tl.to(
+        ".images .left",
+        {
+          marginTop: "150%",
+          duration: 1,
+        },
+        0
+      );
+    });
+    ScrollTrigger.refresh();
   });
 
   onCleanup(() => {
-    timeline.reverse();
+    ctx.revert();
   });
 
   return (
-    <>
-      <section class="spacer"></section>
-      <section class="w-full p-5 sticky-container" ref={stickyContainer}>
-        <div
-          class="w-full sticky-content overflow-hidden"
-          ref={(el) => (section2Ref = el)}
-        >
-          <div class="flex items-center h-full w-full justify-between">
-            <div class="w-[45%] space-y-2.5">
-              <div>
-                <h2 class="text-center font-semibold lg:text-[30px] md:text-[28px] text-[25px]">
-                  The Metering Hardware
-                </h2>
-                <p class="text-center text-base sm:text-lg font-medium mt-2">
-                  Maxwell 1.0.7
-                </p>
-              </div>
-              <div class="w-full grid grid-cols-1 gap-7 h-fit">
-                <div class="w-full space-y-3">
-                  <p>
-                    Designed specifically to bridge the gap between distributed
-                    energy resources (such as rooftop solar assets) and the
-                    web3.0 economy. Its data is crucial for accurate tracking,
-                    verification, and billing of the energy transactions within
-                    the protocol.
-                  </p>
-                </div>
-
-                <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                  <div class="w-full space-y-3">
-                    <h3 class="text-lg sm:text-xl font-bold">Security</h3>
-                    <p>
-                      Utilizes ED25519 elliptical curve digital signatures to
-                      ensure data integrity. Keys are stored on a
-                      tamper-resistant crypto chip that self-destructs if
-                      accessed externally. These security measures prevent
-                      unauthorized access and tampering, guaranteeing accurate
-                      and trustworthy data.
-                    </p>
-                  </div>
-                  <div class="w-full space-y-3">
-                    <h3 class="text-lg sm:text-xl font-bold">Network</h3>
-                    <p>
-                      Operates wirelessly via LoRaWAN (LongFi), a long-range,
-                      low-power protocol designed for IoT with range up to 3 km.
-                      Ideal for remote installations where traditional wireless
-                      communication is limited or costly.
-                    </p>
-                  </div>
-                </div>
-              </div>
+    <div class="container wrapper">
+      <div class="section_1"></div>
+      <div class="section_2" ref={section2Ref}>
+        <div class="cont-flex">
+          <div class="content">
+            <h1>Photography art</h1>
+            <p>
+              Photography is the art, application, and practice of creating
+              images by recording light, either electronically by means of an
+              image sensor, or chemically by means of a light-sensitive material
+              such as photographic film.
+            </p>
+          </div>
+          <div class="images">
+            <div class="left">
+              <div class="img"></div>
+              <div class="img"></div>
+              <div class="img"></div>
+              <div class="img"></div>
+              <div class="img"></div>
             </div>
-            <div
-              class="images h-[100vh] flex items-center w-[45%] justify-between"
-              style={{
-                mask: "linear-gradient(transparent, white 10%, white 85%, transparent)",
-              }}
-              id="images"
-            >
-              <div
-                class="left flex flex-col m-[2rem] will-change-transform gap-[2rem]"
-                ref={leftCol}
-              >
-                <For each={[1, 2, 3, 4, 5]}>
-                  {(item) => (
-                    <div
-                      class={`aspect-square w-[250px] h-[250px] flex justify-center items-center bg-amber-700 text-white`}
-                    >
-                      {item}
-                    </div>
-                  )}
-                </For>
-              </div>
-              {/*Right*/}
-              <div
-                class="right flex flex-col m-[2rem] will-change-transform gap-[2rem] mt-[40rem]"
-                ref={rightCol}
-              >
-                <For each={[1, 2, 3, 4, 5].reverse()}>
-                  {(item) => (
-                    <div
-                      class={`aspect-square w-[250px] h-[250px] flex justify-center items-center bg-amber-700 text-white`}
-                    >
-                      {item}
-                    </div>
-                  )}
-                </For>
-              </div>
+            <div class="right">
+              <div class="img"></div>
+              <div class="img"></div>
+              <div class="img"></div>
+              <div class="img"></div>
+              <div class="img"></div>
             </div>
           </div>
         </div>
-      </section>
-      <section class="spacer"></section>
-    </>
+      </div>
+      <div class="section_3"></div>
+    </div>
   );
 };
 export default Marquee;
